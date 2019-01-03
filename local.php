2379e021a3a0d76256326d9e59dc39cc2849850f
spetruzza <steve.petruzza@gmail.com>
2019-01-03 10:32:11 -0700

    comments on local configuration file

--- local.php
+++ local.php
@@ -1,5 +1,10 @@
 <?php 
+    // This is a local site configuration file
+	
+	// Admin credentials for this server
     $admin_user="admin"; 
-    $admin_password="solopizza";	
+    $admin_password="password";
+	
+	// Default server config file location (on the server's filesystem)	
     $default_config_file="/home/visus/visus.config";
 ?>

03a1b5b7616f8657d983fd1d1a134d0df546d62f
spetruzza <spetruzza@sci.utah.edu>
2019-01-30 11:56:12 -0700

    fix path name single conversion

--- convert.php
+++ convert.php
@@ -22,7 +22,6 @@
 		        $ext = $file_parts['extension'];
 			$fname = $file_parts['filename'];
 				
-			$fname="";
 			/*$dp = opendir ($dir);
 			while ($f = readdir($dp)){
 				//echo $dir."/".$f."\n";
@@ -36,7 +35,7 @@
 				}
 			}*/
 			
-			$foldername=basename($dir);
+			//$foldername=basename($dir);
 			
 			$iX=intval($X);
 			$iY=intval($Y);
@@ -50,12 +49,12 @@
 			date_default_timezone_set('America/Denver');
 			$current_date = date('Y-m-d H:i:s');
 			
-			$params = array('visus_exe' => $visus_exe, 'dir' => $dir, 'fname' => $foldername, 'img' => $img, 'dim' => $dim, 'dtype' => $dtype_full);
+			$params = array('visus_exe' => $visus_exe, 'dir' => $dir, 'fname' => $fname, 'img' => $img, 'dim' => $dim, 'dtype' => $dtype_full);
             $json_params = json_encode($params);
 			//echo  $json_params;
 			
 			// visus_exe data_dir filename input_file dim dtype 
-			$cmd="scripts/convert_single.sh $visus_exe $dir $foldername $img \"$dim\" \"$dtype_full\"";
+			$cmd="scripts/convert_single.sh $visus_exe $dir $fname $img \"$dim\" \"$dtype_full\"";
 			
 			$logfile="$dir/convert-".strtotime($current_date).".log";
 			$pidfile="$dir/convert-".strtotime($current_date).".pid";
@@ -77,7 +76,7 @@
 			
 			   $sql =<<<EOF
 				  INSERT INTO conversion (name,type,status,params,pid,time,logfile)
-				  VALUES ('$foldername', '$ctype', 'STARTED', '$json_params', '$pid', '$current_date','$logfile'); 
+				  VALUES ('$fname', '$ctype', 'STARTED', '$json_params', '$pid', '$current_date','$logfile'); 
 EOF;
 			
 			   $ret = $db->exec($sql);

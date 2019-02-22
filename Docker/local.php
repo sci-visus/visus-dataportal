481cb0ae299d4e543f143a9c6f0d331cec53b2c6
spetruzza <steve.petruzza@gmail.com>
2019-02-21 20:54:16 -0700

    separate drive for converted data

--- Docker/local.php
+++ Docker/local.php
@@ -1,3 +1,4 @@
+
 <?php 
   // This is a local site configuration file
 	
@@ -7,13 +8,20 @@
 	
   // Default server config file location (on the server's filesystem)	
   $default_config_file="/home/OpenVisus/visus.config";
-  
-  // Data directory
+
+  // mod_visus_url
+  $mod_visus_url="http://localhost:8080/mod_visus?";
+ 
+  /**** The following are internal settings, do not change
+         unless you know what you are doing                 ****/
+
+  // Data input directory
   $data_dir="/data";
+
+  // Data output directory
+  $out_data_dir="/converted";
     
   // visus executable
   $visus_exe="/usr/local/lib/python3.5/dist-packages/OpenVisus/bin/visus";
   
-  // mod_visus_url
-  $mod_visus_url="http://localhost:8080/mod_visus?";
-?>
+ ?>
